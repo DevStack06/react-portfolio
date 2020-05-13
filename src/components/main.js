61f46f1d67bb18b7057b9da93f7a15b7b8ac8045
balram0608 <balram0608@gmail.com>
2020-05-13 22:40:53 +0530

    route fix

--- src/components/main.js
+++ src/components/main.js
@@ -9,7 +9,7 @@ import Resume from "./Resume/resume";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={LandingPage} />
+    <Route path="/" component={LandingPage} />
     <Route path="/aboutme" component={AboutMe} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
