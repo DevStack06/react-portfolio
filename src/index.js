f61d19567c5ca00f95926b8978eee52163668d5d
balram0608 <balram0608@gmail.com>
2020-05-13 23:02:41 +0530

    route fix

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "react-mdl/extra/material.css";
 import "react-mdl/extra/material.js";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 registerServiceWorker();
