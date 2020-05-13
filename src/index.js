e6227f9fadc2cb2929fcfcf789398f399f97ca90
balram0608 <balram0608@gmail.com>
2020-05-13 22:23:05 +0530

    domain  changed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "react-mdl/extra/material.css";
+import "react-mdl/extra/material.js";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-<App />
-  </BrowserRouter>
-  , document.getElementById('root'));
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
